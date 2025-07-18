8d839f4c512932328b3613ae3cacfbbb79e8372f
Jose Augusto Megres de Oliveira <jose.megres@gmail.com>
2025-07-18 18:40:29 -0300

    feat: export types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { RabbitConsumerAdapter } from "./adapters/MessagingConsumerAdapter";
 import { RabbitPublisherAdapter } from "./adapters/MessagingPublisherAdapter";
+import { IMessagingConsumerAdapter } from "./contracts/IMessagingConsumer";
+import { IMessagingPublisherAdapter } from "./contracts/IMessagingPublisher";
 
-
+export type { IMessagingConsumerAdapter, IMessagingPublisherAdapter }
 export { RabbitConsumerAdapter, RabbitPublisherAdapter }
